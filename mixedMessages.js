de1ddd1614167ee713c29428671783d4760077cc
alevy <613aml@gmail.com>
2021-01-25 15:03:10 -0800

    making project outline

---
+++ mixedMessages.js
@@ -0,0 +1,15 @@
+vLib = []
+nLib = []
+adjLib = []
+
+const vGenerator 
+
+const nGenerator
+
+const adjGenerator
+
+
+
+const whatAreYou = () {
+    return `You are a ${adjGenerator} ${nGenerator} ${vGenerator} in the ${nGenerator}`
+}
